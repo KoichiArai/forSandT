427e5d207e8b781ba9ea09cee3292b2b50c5f33b
KoichiroArai <arai.no.8260@gmail.com>
2024-03-09 15:00:08 +0900

    Create abstractRecruitReader.py

---
+++ abstractRecruitReader.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+def main():
+    pass
+
+class abstractRecruitReader():
+    @abstractmethod
+    def shopSalaryMonth(self):
+        pass
+    
+    @abstractmethod
+    def shopAddress(self):
+        pass
+
+    @abstractmethod
+    def shopName(self):
+        pass
